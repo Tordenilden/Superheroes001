fd95471e409ac2f4b644b1fdea83d8fda59f8f37
Tordenliden <fls@tec.dk>
2023-08-08 10:13:19 +0200

    tuesday

--- Superheroes001/Controllers/HeroesController.cs
+++ Superheroes001/Controllers/HeroesController.cs
@@ -6,17 +6,49 @@ using Superheroes001.Entities;
 
 namespace Superheroes001.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class HeroesController : ControllerBase
     {
+        /// <summary>
+        /// Tuesday
+        /// List Hero => LINQ 3-7
+        /// Lambda
+        /// - Opgaver
+        /// 
+        /// </summary>
         DatabaseContext context { get; set; }
+
+        List<Hero> heroes;
+
         // the parametre in this method is called Dependency Injection
         public HeroesController(DatabaseContext bandit)
         {
             context = bandit;
+            heroes = new List<Hero>()
+            {
+                new Hero{Id=1, Name = "Phill", DebutYear= Convert.ToDateTime("12-12-2023"), Place = "Ballerup" ,
+                    RealName = "Julemanden" },
+                new Hero{Id=2, Name = "Anna", DebutYear= Convert.ToDateTime("01-03-2023"), Place = "Grønland" ,
+                    RealName = "Batman" },
+                new Hero{Id=10, Name = "Mai", DebutYear= Convert.ToDateTime("08-08-2023"), Place = "Danmark" ,
+                    RealName = "Superman" },
+            };
         }
         /// <summary>
+        /// 
+
+        #region monday
+
+
+
+
+
+
+
+
+
         /// how to generate sql script: 
         /// add-migration someName
         /// update-database
@@ -72,5 +104,54 @@ namespace Superheroes001.Controllers
         public void Delete(int id)
         {
         }
+
+        #endregion monday
+        #region tuesday
+        // method that loop the list and find Id == 2, foreach , if 
+
+        [HttpGet("MethodWithForeach")]
+        public ActionResult<Hero> methodThatUseForeach()
+        {
+            foreach (var hero in heroes)
+            {
+                if (hero.Id == 2) { return hero; }
+            }
+            return BadRequest();
+        }
+   
+
+        [HttpGet("MethodWithLINQ")]      
+        public ActionResult<Hero> methodThatUseLINQ()
+        {
+            var hero = heroes.Where(hero => hero.Id == 88).FirstOrDefault();
+            return hero;
+        }
+        /// <summary>
+        /// LINQ is used on Collections
+        /// LINQ generate a query but is not Executed
+        /// There are several ways to Execute a LINQ statement
+        /// </summary>
+        /// <returns></returns>
+
+
+        [HttpGet("MethodWithLINQMaxId")]
+        public ActionResult<Hero> methodThatUseLINQMaxId()
+        {
+            var hero = heroes.MaxBy(flødebolle=> flødebolle.Id);
+            return Ok(hero);
+        }
+
+        [HttpGet("MethodWithLINQMaxId002")]
+        public ActionResult<Hero> methodThatUseLINQMaxId002()
+        {
+            var heroId = heroes.Max(flødebolle => flødebolle.Id);
+            var hero = heroes.Where(hero => hero.Id == heroId).FirstOrDefault();
+
+
+            var oneLine = heroes.Where(hero => hero.Id 
+                == heroes.Max(flødebolle => flødebolle.Id)).FirstOrDefault();
+            return Ok(hero);
+        }
+        #endregion tuesday
     }
 }
---
+++ Superheroes001/Entities/Team.cs
@@ -0,0 +1,14 @@
+﻿namespace Superheroes001.Entities
+{
+    public class Team
+    {
+        // I want more than one Hero here
+        // date maybe
+        // 1 - M
+        public List<Hero> HeroList { get; set; }
+        public string Name { get; set; }
+        public string StupidPeople { get; set; }
+        public string Location { get; set; }
+
+    }
+}
